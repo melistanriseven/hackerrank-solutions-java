bf32dbab3dfb475f26e00221b6dfa3798f74a47f
melistanriseven <melistanriseven77@gmail.com>
2022-03-07 22:39:26 +0300

    first commit

---
+++ src/main/java/com/hackerrank/solutions/easy/WelcomeToJava.java
@@ -0,0 +1,9 @@
+package com.hackerrank.solutions.easy;
+
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
